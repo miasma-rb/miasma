f4e753884e5dc56564f097b652b28ca06ecdfe97
Chris Roberts <chrisroberts.code@gmail.com>
2014-10-17 14:45:29 -0700

    Restructure resource since now proper model. Allow instance loading if possible.

--- lib/miasma/models/orchestration/resource.rb
+++ lib/miasma/models/orchestration/resource.rb
@@ -11,38 +11,46 @@ module Miasma
           attribute :name, String, :required => true
           attribute :type, String, :required => true
           attribute :logical_id, [String, Numeric]
+          attribute :state, Symbol, :required => true, :allowed_values => Orchestration::VALID_RESOURCE_STATES
           attribute :status, [String, Symbol], :required => true, :coerce => lambda{|v| v.to_s.to_sym}
           attribute :status_reason, String
           attribute :updated_time, Time
-          attribute :links, Array
 
           attr_reader :stack
 
-          def initalize(stack, args={})
+          def initialize(stack, args={})
             @stack = stack
             super stack.api, args
           end
 
           # @return [Miasma::Types::Model] provides mapped resource class
-          def model
+          def dereference
             # Insert provider to namespace
             provider_const = self.class.name.split('::').insert(3, Utils.camel(api.provider))
-            # Remove current class
-            provider_const.pop
+            provider_const.slice!(-2, 2)
             # Insert mapping constant name and fetch
-            const = provider_const.push(:RESOURCE_MAPPING).inject(Object) do |memo, konst|
+           const = provider_const.push(:RESOURCE_MAPPING).inject(Object) do |memo, konst|
               res = memo.const_get(konst)
               break unless res
               res
             end
-            if(const && const = const[self.type])
-              # Now rebuild from the ground up
-              const.to_s.split('::').insert(3, Utils.camel(api.provider)).inject(Object) do |memo, konst|
-                memo.const_get(konst)
-              end
-            else
+            unless(const[self.type])
               raise KeyError.new "Failed to locate requested mapping! (`#{self.type}`)"
             end
+            const[self.type]
+          end
+
+          # Provide proper instance from resource
+          #
+          # @return [Miasma::Types::Model]
+          def instance
+            info = dereference
+            api.api_for(info[:api]).send(info[:collection]).get(self.id)
+          end
+
+          # Proxy reload action up to the API
+          def perform_reload
+            api.resource_reload(self)
           end
 
           include Utils::Immutable

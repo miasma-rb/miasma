67744192c0eba305c8cbb22f9c889b0277fd05ae
Chris Roberts <code@chrisroberts.org>
2014-10-22 22:09:21 -0700

    Update the file methods

--- lib/miasma/models/storage/file.rb
+++ lib/miasma/models/storage/file.rb
@@ -1,4 +1,5 @@
 require 'miasma'
+require 'stringio'
 
 module Miasma
   module Models
@@ -7,12 +8,26 @@ module Miasma
       # Abstract file
       class File < Types::Collection
 
-        attribute :body, [String, IO], :default => ''
+        attribute :body, IO, :coerce => lambda{|v| StringIO.new(v.to_s) }
         attribute :content_type, String
         attribute :content_disposition, String
         attribute :etag, String
         attribute :last_modified, DateTime
+        attribute :size, Integer
         attribute :metadata, Hash, :coerce => lambda{|o| o.to_smash}
+
+        # @return [Bucket] parent bucket
+        attr_reader :bucket
+
+        # Create a new instance
+        #
+        # @param bucket [Bucket]
+        # @param args [Hash]
+        # @return [self]
+        def initialize(bucket, args={})
+          @bucket = bucket
+          super bucket.api, args
+        end
 
         # Return files matching given filter
         #
--- lib/miasma/models/storage/files.rb
+++ lib/miasma/models/storage/files.rb
@@ -7,6 +7,18 @@ module Miasma
       # Abstract file collection
       class Files < Types::Collection
 
+        # @return [Bucket] parent bucket
+        attr_reader :bucket
+
+        # Create new instance
+        #
+        # @param bucket [Bucket] parent bucket
+        # @return [self]
+        def initialize(bucket)
+          @bucket = bucket
+          super bucket.api
+        end
+
         # Return files matching given filter
         #
         # @param options [Hash] filter options
@@ -14,6 +26,18 @@ module Miasma
         # @option options [String] :prefix key prefix
         def filter(options={})
           raise NotImplementedError
+        end
+
+        # @return [File] new unsaved instance
+        def build(args={})
+          File.new(api, args.to_smash)
+        end
+
+        protected
+
+        # @return [Array<File>]
+        def perform_population
+          api.file_all(bucket)
         end
 
       end

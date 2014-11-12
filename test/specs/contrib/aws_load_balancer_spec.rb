d35966d423a61790cba0dec21a8ddd49a70b1c80
Chris Roberts <chrisroberts.code@gmail.com>
2014-11-11 20:36:55 -0800

    Add load balancer specs for aws

---
+++ test/specs/contrib/aws_load_balancer_spec.rb
@@ -0,0 +1,23 @@
+# require 'miasma/contrib/aws'
+
+# describe Miasma::Models::LoadBalancer::Aws do
+
+#   before do
+#     @load_balancer = Miasma.api(
+#       :type => :load_balancer,
+#       :provider => :aws,
+#       :credentials => {
+#         :aws_access_key_id => ENV['MIASMA_AWS_ACCESS_KEY_ID'],
+#         :aws_secret_access_key => ENV['MIASMA_AWS_SECRET_ACCESS_KEY'],
+#         :aws_region => ENV['MIASMA_AWS_REGION']
+#       }
+#     )
+#   end
+
+#   let(:load_balancer){ @load_balancer }
+#   let(:build_args){ Smash.new(:name => 'miasma-test-load-balancer') }
+#   let(:cassette_prefix){ 'aws_load_balancer' }
+
+#   instance_exec(&MIASMA_LOAD_BALANCER_ABSTRACT)
+
+# end

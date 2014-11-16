24929fda91734ee7ee73bae359e204451521c399
Chris Roberts <chrisroberts.code@gmail.com>
2014-11-16 07:46:15 -0800

    Enable load balancer spec for aws. Provide required listeners setup.

--- test/specs/contrib/aws_load_balancer_spec.rb
+++ test/specs/contrib/aws_load_balancer_spec.rb
@@ -1,23 +1,33 @@
-# require 'miasma/contrib/aws'
+require 'miasma/contrib/aws'
 
-# describe Miasma::Models::LoadBalancer::Aws do
+describe Miasma::Models::LoadBalancer::Aws do
 
-#   before do
-#     @load_balancer = Miasma.api(
-#       :type => :load_balancer,
-#       :provider => :aws,
-#       :credentials => {
-#         :aws_access_key_id => ENV['MIASMA_AWS_ACCESS_KEY_ID'],
-#         :aws_secret_access_key => ENV['MIASMA_AWS_SECRET_ACCESS_KEY'],
-#         :aws_region => ENV['MIASMA_AWS_REGION']
-#       }
-#     )
-#   end
+  before do
+    @load_balancer = Miasma.api(
+      :type => :load_balancer,
+      :provider => :aws,
+      :credentials => {
+        :aws_access_key_id => ENV['MIASMA_AWS_ACCESS_KEY_ID'],
+        :aws_secret_access_key => ENV['MIASMA_AWS_SECRET_ACCESS_KEY'],
+        :aws_region => ENV['MIASMA_AWS_REGION']
+      }
+    )
+  end
 
-#   let(:load_balancer){ @load_balancer }
-#   let(:build_args){ Smash.new(:name => 'miasma-test-load-balancer') }
-#   let(:cassette_prefix){ 'aws_load_balancer' }
+  let(:load_balancer){ @load_balancer }
+  let(:build_args){
+    Smash.new(
+      :name => 'miasma-test-load-balancer',
+      :listeners => [
+        :load_balancer_port => 80,
+        :instance_port => 80,
+        :protocol => 'HTTP',
+        :instance_protocol => 'HTTP'
+      ]
+    )
+  }
+  let(:cassette_prefix){ 'aws_load_balancer' }
 
-#   instance_exec(&MIASMA_LOAD_BALANCER_ABSTRACT)
+  instance_exec(&MIASMA_LOAD_BALANCER_ABSTRACT)
 
-# end
+end

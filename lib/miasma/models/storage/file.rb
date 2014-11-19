c98ae342b6e8ce5e43696a5af1895e51e9227f49
Chris Roberts <chrisroberts.code@gmail.com>
2014-11-19 07:14:05 -0800

    Include encoding attribute. Handle `body` directly. Add api proxy helpers. Force reload on missing attributes.

--- lib/miasma/models/storage/file.rb
+++ lib/miasma/models/storage/file.rb
@@ -9,13 +9,15 @@ module Miasma
       class File < Types::Model
 
         attribute :name, String, :required => true
-        attribute :body, IO, :coerce => lambda{|v| StringIO.new(v.to_s) }
         attribute :content_type, String
         attribute :content_disposition, String
+        attribute :content_encoding, String
         attribute :etag, String
         attribute :updated, Time, :coerce => lambda{|t| Time.parse(t.to_s)}
         attribute :size, Integer
         attribute :metadata, Smash, :coerce => lambda{|o| o.to_smash}
+
+        on_missing :reload
 
         # @return [Bucket] parent bucket
         attr_reader :bucket
@@ -37,6 +39,43 @@ module Miasma
         # @option options [String] :prefix key prefix
         def filter(options={})
           raise NotImplementedError
+        end
+
+        # @return [IO-ish]
+        # @note object returned will provide #readpartial
+        def body
+          unless(attributes[:body])
+            data[:body] ||= api.file_body(self)
+          end
+          attributes[:body]
+        end
+
+        # Set file body
+        #
+        # @param io [IO, String]
+        # @return [IO]
+        def body=(io)
+          unless(io.is_a?(IO))
+            io = StringIO.new(io)
+          end
+          dirty[:body] = io
+        end
+
+        protected
+
+        # Proxy reload action up to the API
+        def perform_reload
+          api.file_reload(self)
+        end
+
+        # Proxy save action up to the API
+        def perform_save
+          api.file_save(self)
+        end
+
+        # Proxy destroy action up to the API
+        def perform_destroy
+          api.file_destroy(self)
         end
 
       end

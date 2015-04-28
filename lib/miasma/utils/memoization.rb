f60d48ef5c47c978164e172e1ad18ededd63b3f4
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-28 06:18:09 -0700

    Use common modules provided via bogo library (#10)

--- lib/miasma/utils/memoization.rb
+++ lib/miasma/utils/memoization.rb
@@ -3,53 +3,6 @@ require 'miasma'
 module Miasma
   module Utils
     # Memoization helpers
-    module Memoization
-
-      # Memoize data
-      #
-      # @param key [String, Symbol] identifier for data
-      # @param direct [Truthy, Falsey] direct skips key prepend of object id
-      # @yield block to create data
-      # @yieldreturn data to memoize
-      # @return [Object] data
-      def memoize(key, direct=false)
-        unless(direct)
-          key = "#{self.object_id}_#{key}"
-        end
-        unless(_memo.has_key?(key))
-          _memo[key] = yield
-        end
-        _memo[key]
-      end
-
-      def _memo
-        Thread.current[:miasma_memoization] ||= Smash.new
-      end
-
-      # Remove memoized value
-      #
-      # @param key [String, Symbol] identifier for data
-      # @param direct [Truthy, Falsey] direct skips key prepend of object id
-      # @return [NilClass]
-      def unmemoize(key, direct=false)
-        unless(direct)
-          key = "#{self.object_id}_#{key}"
-        end
-        _memo.delete(key)
-      end
-
-      # Remove all memoized values
-      #
-      # @return [TrueClass]
-      def clear_memoizations!
-        _memo.keys.find_all do |key|
-          key.to_s.start_with?("#{self.object_id}_")
-        end.each do |key|
-          _memo.delete(key)
-        end
-        true
-      end
-
-    end
+    Memoization = Bogo::Memoization
   end
 end

899e15d302c8bfeaab6902761328f80e5b54d965
Chris Roberts <code@chrisroberts.org>
2016-02-12 13:30:25 -0800

    Split the name of the class, not the actual constant

--- lib/miasma/utils/api_methoding.rb
+++ lib/miasma/utils/api_methoding.rb
@@ -9,7 +9,7 @@ module Miasma
       # a given action
       def api_method_for(action)
         klass = self.respond_to?(:model) ? model : self.class
-        m_name = "#{Bogo::Utility.snake(klass.split('::').last)}_#{action}"
+        m_name = "#{Bogo::Utility.snake(klass.name.split('::').last)}_#{action}"
         self.api.respond_to?(m_name) ? m_name : nil
       end
 
